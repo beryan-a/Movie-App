5f97ebc70ad6d21ea573998f4fce739d740852ea
Beryan Albeik <beryanalbeik@gmail.com>
2026-08-18 13:13:07 +0300

    test changes in data

--- Backend/obj/Debug/net10.0/movie_Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/movie_Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movie_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4d57abdc91d5b3d8c4f497cbd06f3d510e84f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacb90f5007fa0fb1cb123e0ba1b8d1e0484fc1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("movie_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movie_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
